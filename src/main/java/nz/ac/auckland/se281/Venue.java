e6463117bb664fb16e547642fb91bd8dc4b1ca10
Reynhard Buitendach <reynhard.buitendach@gmail.com>
2024-03-21 03:03:33 +1300

    created venue class to create instances of venues

---
+++ src/main/java/nz/ac/auckland/se281/Venue.java
@@ -0,0 +1,16 @@
+package nz.ac.auckland.se281;
+
+public class Venue {
+
+  private String venueName;
+  private String venueCode;
+  private int capacity;
+  private int hireFee;
+
+  public Venue(String venueName, String venueCode, int capacity, int hireFee) {
+    this.venueName = venueName;
+    this.venueCode = venueCode;
+    this.capacity = capacity;
+    this.hireFee = hireFee;
+  }
+}
